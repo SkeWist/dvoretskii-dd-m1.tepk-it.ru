ba10f83da9db2b338a6088d999cfc05592d8727c
dvoretskii-dd <jojo.denis@mail.ru>
2025-06-07 13:37:09 +0700

    style, layouts, welcome

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="">
+    <link rel="icon" href="">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <div>
+            <img src="" alt="Логотип"/>
+        </div>
+        <ul>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    @yield('content')
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
